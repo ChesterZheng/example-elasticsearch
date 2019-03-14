ab2dd261870fe950d921b3d36934f7e9f8454291
zhengxiaochen <chester.zheng1005@gmail.com>
2019-03-14 14:10:44 +0800

    使用boost控制搜索结果的权重 boost的值越大，权重越大，结果排序越靠前

---
+++ src/main/java/com/example/elasticsearch/forum/query/TestBoost.java
@@ -0,0 +1,49 @@
+package com.example.elasticsearch.forum.query;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+
+import com.example.elasticsearch.util.ElasticSearchUtil;
+
+/**
+ * es boost
+ * 
+ * @Author Chester_Zheng
+ * @Date 2019年3月14日下午1:52:56
+ * @Tags
+ */
+public class TestBoost {
+
+	public static void main(String[] args) throws Exception {
+		// 初始化
+		TransportClient client = ElasticSearchUtil.init();
+		System.out.println("sample1的结果");
+		// 搜索标题中包含java，同时，如果标题中包含hadoop或elasticsearch就优先被搜索出来
+		// 如果一个帖子包含java hadoop，一个帖子包含java elasticsearch
+		// 那么hadoop的帖子要比elasticsearch的帖子优先被搜索出来
+		TestBoost.sample1(client);
+		// 销毁
+		ElasticSearchUtil.destory(client);
+	}
+
+	/*
+	 * 通过使用boost，来控制搜索结果的权重
+	 * boost大的relevance score会更高
+	 * boost默认为1
+	 */
+	public static void sample1(TransportClient client) throws Exception {
+		SearchResponse response = client.prepareSearch("forum").setTypes("article")
+				.setQuery(QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("titile", "java"))
+						.should(QueryBuilders.matchQuery("title", "hadoop").boost(3))
+						.should(QueryBuilders.matchQuery("title", "elasticsearch").boost(2)))
+				.get();
+		SearchHit[] hits = response.getHits().getHits();
+		for (int i = 0; i < hits.length; i++) {
+			String sourceAsString = hits[i].getSourceAsString();
+			System.out.println(sourceAsString);
+		}
+	}
+
+}
