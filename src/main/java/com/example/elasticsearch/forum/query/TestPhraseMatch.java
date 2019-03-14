04e54eb340d21f1071077d2d008b0f56e112ab73
zhengxiaochen <chester.zheng1005@gmail.com>
2019-03-14 18:05:48 +0800

    example-elasticsearch java api

---
+++ src/main/java/com/example/elasticsearch/forum/query/TestPhraseMatch.java
@@ -0,0 +1,36 @@
+package com.example.elasticsearch.forum.query;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import com.example.elasticsearch.util.ElasticSearchUtil;
+
+/**
+ * es match phrase
+ * 
+ * @Author Chester_Zheng
+ * @Date 2019年3月14日下午5:13:25
+ * @Tags
+ */
+public class TestPhraseMatch {
+
+	public static void main(String[] args) throws Exception {
+		// 初始化
+		TransportClient client = ElasticSearchUtil.init();
+		TestPhraseMatch.sample(client);
+		// 销毁
+		ElasticSearchUtil.destory(client);
+	}
+
+	/*
+	 * 短语匹配原理 match_phrase在搜索时, 会将java spark拆分成java和spark
+	 * 然后去docement中搜索，满足java的term position比spark的term position小1的将会被搜索出来
+	 */
+	public static void sample(TransportClient client) throws Exception {
+		SearchResponse response = client.prepareSearch("forum").setTypes("article")
+				.setQuery(QueryBuilders.matchPhraseQuery("content", "java spark")).get();
+		ElasticSearchUtil.showResults(response);
+	}
+
+}
--- src/main/java/com/example/elasticsearch/forum/query/TestProximityMatchWithSlop.java
+++ src/main/java/com/example/elasticsearch/forum/query/TestProximityMatchWithSlop.java
@@ -7,34 +7,29 @@ import org.elasticsearch.index.query.QueryBuilders;
 import com.example.elasticsearch.util.ElasticSearchUtil;
 
 /**
- * es match phrase
- * 
+ * es slop
  * @Author Chester_Zheng
- * @Date 2019年3月14日下午5:13:25
+ * @Date 2019年3月14日下午5:38:36
  * @Tags
  */
-public class TestMatchPhrase {
+public class TestProximityMatchWithSlop {
 
 	public static void main(String[] args) throws Exception {
 		// 初始化
 		TransportClient client = ElasticSearchUtil.init();
-		TestMatchPhrase.sample(client);
+		TestProximityMatchWithSlop.sample(client);
 		// 销毁
 		ElasticSearchUtil.destory(client);
 	}
 
-	/**
-	 * 
-	 * @Author Chester_Zheng
-	 * @Description 短语匹配
-	 * @Date 2019年3月14日下午5:25:08
-	 * @Tags  @param client
-	 * @Tags  @throws Exception
-	 * @ReturnType void
+	/*
+	 * slop参数含义
+	 * term最多可以移动几次去匹配document
+	 * term之间的距离越近, relevance score越高
 	 */
 	public static void sample(TransportClient client) throws Exception {
 		SearchResponse response = client.prepareSearch("forum").setTypes("article")
-				.setQuery(QueryBuilders.matchPhraseQuery("content", "java spark")).get();
+				.setQuery(QueryBuilders.matchPhraseQuery("content", "java best").slop(15)).get();
 		ElasticSearchUtil.showResults(response);
 	}
 
