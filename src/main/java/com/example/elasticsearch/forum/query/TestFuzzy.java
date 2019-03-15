345652fdcb7fa0ff74de9566ca514b69418f0c37
zhengxiaochen <chester.zheng1005@gmail.com>
2019-03-15 17:22:55 +0800

    example-elasticsearch java api

--- src/main/java/com/example/elasticsearch/forum/query/TestFuzzy.java
+++ src/main/java/com/example/elasticsearch/forum/query/TestFuzzy.java
@@ -3,6 +3,7 @@ package com.example.elasticsearch.forum.query;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
 
 import com.example.elasticsearch.util.ElasticSearchUtil;
@@ -28,18 +29,31 @@ public class TestFuzzy {
 	public static void main(String[] args) throws Exception {
 		// 初始化
 		TransportClient client = ElasticSearchUtil.init();
-		TestFuzzy.sample(client);
+		System.out.println("sample1的结果");
+		TestFuzzy.sample1(client);
+		System.out.println("======================================================================");
+		System.out.println("sample2的结果");
+		TestFuzzy.sample2(client);
 		// 销毁
 		ElasticSearchUtil.destory(client);
 	}
 
 	/*
-	 * 使用fuzzy模糊查询会自动尝试将搜索词进行纠错, 然后再去倒排索引中查询
-	 * fuzziness, 指定你的搜索词最多可以纠正几个字母, 默认值是2
+	 * 使用fuzzy模糊查询会自动尝试将搜索词进行纠错, 然后再去倒排索引中查询 fuzziness, 指定你的搜索词最多可以纠正几个字母, 默认值是2
 	 */
-	public static void sample(TransportClient client) throws Exception {
+	public static void sample1(TransportClient client) throws Exception {
 		SearchResponse response = client.prepareSearch("my_index").setTypes("my_type")
 				.setQuery(QueryBuilders.fuzzyQuery("text", "surprize").fuzziness(Fuzziness.TWO)).get();
+		ElasticSearchUtil.showResults(response);
+	}
+
+	/*
+	 * 下面的方式更为常用
+	 */
+	public static void sample2(TransportClient client) throws Exception {
+		SearchResponse response = client.prepareSearch("my_index").setTypes("my_type").setQuery(
+				QueryBuilders.matchQuery("text", "SURPRIZE ME!").fuzziness(Fuzziness.AUTO).operator(Operator.AND))
+				.get();
 		ElasticSearchUtil.showResults(response);
 	}
 
