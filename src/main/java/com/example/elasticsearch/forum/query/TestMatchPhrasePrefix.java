d78228ce8a6ec427b904900be65b6da6ba9bee17
zhengxiaochen <chester.zheng1005@gmail.com>
2019-03-15 14:32:47 +0800

    example-elasticsearch java api

---
+++ src/main/java/com/example/elasticsearch/forum/query/TestMatchPhrasePrefix.java
@@ -0,0 +1,37 @@
+package com.example.elasticsearch.forum.query;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import com.example.elasticsearch.util.ElasticSearchUtil;
+
+/**
+ * es match phrase prefix 搜索推荐
+ * 
+ * @Author Chester_Zheng
+ * @Date 2019年3月15日下午1:47:11
+ * @Tags
+ */
+public class TestMatchPhrasePrefix {
+
+	public static void main(String[] args) throws Exception {
+		// 初始化
+		TransportClient client = ElasticSearchUtil.init();
+		TestMatchPhrasePrefix.sample(client);
+		// 销毁
+		ElasticSearchUtil.destory(client);
+	}
+
+	/*
+	 * match_phrase_prefix的原理和phrase_match类似 唯一的区别就是把搜索中使用的最后一个term作为前缀去倒排索引中搜索
+	 * slop最多可以移动多少位来匹配term max_expansions 按下面示例中所查询的term中的s开头的单词,
+	 * 最多匹配多少个term就不再继续查询倒排索引
+	 * match_phrase_prefix性能可能会很差
+	 */
+	public static void sample(TransportClient client) throws Exception {
+		SearchResponse response = client.prepareSearch("forum").setTypes("article")
+				.setQuery(QueryBuilders.matchPhrasePrefixQuery("title", "this is s").slop(10).maxExpansions(50)).get();
+		ElasticSearchUtil.showResults(response);
+	}
+}
---
+++ src/main/java/com/example/elasticsearch/forum/query/TestNgram.java
@@ -0,0 +1,82 @@
+package com.example.elasticsearch.forum.query;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import com.example.elasticsearch.util.ElasticSearchUtil;
+
+public class TestNgram {
+
+	// 准备测试数据
+	// PUT my_index
+	// {
+	// "settings": {
+	// "analysis": {
+	// "filter": {
+	// "autocomplete_filter":{
+	// "type": "edge_ngram",
+	// "min_gram": 1,
+	// "max_gram": 20
+	// }
+	// },
+	// "analyzer": {
+	// "autocomplete":{
+	// "type": "custom",
+	// "tokenizer": "standard",
+	// "filter":[
+	// "lowercase",
+	// "autocomplete_filter"
+	// ]
+	// }
+	// }
+	// }
+	// }
+	// }
+	// PUT my_index/_mapping/my_type
+	// {
+	// "properties": {
+	// "title":{
+	// "type": "string",
+	// "analyzer": "autocomplete",
+	// "search_analyzer": "standard"
+	// }
+	// }
+	// }
+	// PUT my_index/my_type/1
+	// {
+	// "title": "hello world"
+	// }
+	// PUT my_index/my_type/2
+	// {
+	// "title": "hello we"
+	// }
+	// PUT my_index/my_type/3
+	// {
+	// "title": "hello win"
+	// }
+	// PUT my_index/my_type/4
+	// {
+	// "title": "hello dog"
+	// }
+
+	public static void main(String[] args) throws Exception {
+		// 初始化
+		TransportClient client = ElasticSearchUtil.init();
+		TestNgram.sample(client);
+		// 销毁
+		ElasticSearchUtil.destory(client);
+	}
+
+	/*
+	 * 推荐使用match_phrase和ngram去做搜索推荐功能 如果只用match的话, 是全文检索, 按下面的示例来讲,
+	 * 包含hello的也会被搜索出来 而如果用match_phrase, 是要求每个term都要出现,并且term之间的postition相差1
+	 * 
+	 */
+	public static void sample(TransportClient client) throws Exception {
+		SearchResponse response = client.prepareSearch("my_index").setTypes("my_type")
+				.setQuery(QueryBuilders.matchPhraseQuery("title", "hello w")).get();
+		ElasticSearchUtil.showResults(response);
+	}
+
+}
