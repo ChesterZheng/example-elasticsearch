254450c5e3fea024f4712605749c06ec99971d94
zhengxiaochen <chester.zheng1005@gmail.com>
2019-03-20 15:59:48 +0800

    example-elasticsearch java api

--- src/main/java/com/example/elasticsearch/tvs/aggregator/TestBucketAndMetricsMax.java
+++ src/main/java/com/example/elasticsearch/tvs/aggregator/TestBucketAndMetricsMax.java
@@ -1,9 +1,48 @@
 package com.example.elasticsearch.tvs.aggregator;
 
+import java.util.List;
+import java.util.Map;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms.Bucket;
+import org.elasticsearch.search.aggregations.metrics.max.Max;
+
+import com.example.elasticsearch.util.ElasticSearchUtil;
+
 public class TestBucketAndMetricsMax {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		// 初始化
+		TransportClient client = ElasticSearchUtil.init();
+		TestBucketAndMetricsMax.sample(client);
+		// 销毁
+		ElasticSearchUtil.destory(client);
+	}
 
+	/*
+	 * 相当于MySQL的查询语句 SELECT MAX(PRICE) FROM TVS.SALES GROUP BY COLOR
+	 */
+	public static void sample(TransportClient client) throws Exception {
+		String groupByColorStr = "group_by_color";
+		String maxPriceStr = "max_price";
+		SearchResponse response = client.prepareSearch("tvs").setTypes("sales")
+				.addAggregation(AggregationBuilders.terms(groupByColorStr).field("color")
+						.subAggregation(AggregationBuilders.max(maxPriceStr).field("price")))
+				.get();
+		Map<String, Aggregation> aggsMap = response.getAggregations().asMap();
+		StringTerms groupByColor = (StringTerms) aggsMap.get(groupByColorStr);
+		System.out.println(groupByColor.getName());
+		List<Bucket> groupByColorBucketList = groupByColor.getBuckets();
+		for (int m = 0; m < groupByColorBucketList.size(); m++) {
+			Map<String, Aggregation> subAggsMap = groupByColorBucketList.get(m).getAggregations().asMap();
+			Max maxPrice = (Max) subAggsMap.get(maxPriceStr);
+			System.out.println(groupByColorBucketList.get(m).getKey() + "="
+					+ groupByColorBucketList.get(m).getDocCount() + "=" + maxPrice.getValue());
+		}
 	}
 
 }
--- src/main/java/com/example/elasticsearch/tvs/aggregator/TestBucketAndMetricsSum.java
+++ src/main/java/com/example/elasticsearch/tvs/aggregator/TestBucketAndMetricsSum.java
@@ -9,29 +9,29 @@ import org.elasticsearch.search.aggregations.Aggregation;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms.Bucket;
-import org.elasticsearch.search.aggregations.metrics.min.Min;
+import org.elasticsearch.search.aggregations.metrics.sum.Sum;
 
 import com.example.elasticsearch.util.ElasticSearchUtil;
 
-public class TestBucketAndMetricsMin {
+public class TestBucketAndMetricsSum {
 
 	public static void main(String[] args) throws Exception {
 		// 初始化
 		TransportClient client = ElasticSearchUtil.init();
-		TestBucketAndMetricsMin.sample(client);
+		TestBucketAndMetricsSum.sample(client);
 		// 销毁
 		ElasticSearchUtil.destory(client);
 	}
 
 	/*
-	 * 相当于MySQL的查询语句 SELECT MIN(PRICE) FROM TVS.SALES GROUP BY COLOR
+	 * 相当于MySQL的查询语句 SELECT SUM(PRICE) FROM TVS.SALES GROUP BY COLOR
 	 */
 	public static void sample(TransportClient client) throws Exception {
 		String groupByColorStr = "group_by_color";
-		String minPriceStr = "min_price";
+		String sumPriceStr = "sum_price";
 		SearchResponse response = client.prepareSearch("tvs").setTypes("sales")
 				.addAggregation(AggregationBuilders.terms(groupByColorStr).field("color")
-						.subAggregation(AggregationBuilders.min(minPriceStr).field("price")))
+						.subAggregation(AggregationBuilders.sum(sumPriceStr).field("price")))
 				.get();
 		Map<String, Aggregation> aggsMap = response.getAggregations().asMap();
 		StringTerms groupByColor = (StringTerms) aggsMap.get(groupByColorStr);
@@ -39,9 +39,10 @@ public class TestBucketAndMetricsMin {
 		List<Bucket> groupByColorBucketList = groupByColor.getBuckets();
 		for (int m = 0; m < groupByColorBucketList.size(); m++) {
 			Map<String, Aggregation> subAggsMap = groupByColorBucketList.get(m).getAggregations().asMap();
-			Min minPrice = (Min) subAggsMap.get(minPriceStr);
+			Sum sumPrice = (Sum) subAggsMap.get(sumPriceStr);
 			System.out.println(groupByColorBucketList.get(m).getKey() + "="
-					+ groupByColorBucketList.get(m).getDocCount() + "=" + minPrice.getValue());
+					+ groupByColorBucketList.get(m).getDocCount() + "=" + sumPrice.getValue());
 		}
 	}
+
 }
